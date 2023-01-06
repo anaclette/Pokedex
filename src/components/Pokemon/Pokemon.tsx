b391b89ba0baa25826212ddd6a58055f1214912d
ana.laclau <ana.laclau@mediamonks.com>
2023-01-06 17:15:13 -0300

    first commit

---
+++ src/components/Pokemon/Pokemon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from './pokemon.style';
+
+export const Pokemon = () => {
+  return (
+    <View>
+      <Text style={styles.title}>Pokémon</Text>
+    </View>
+  );
+};
---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Pokemon from '../../components/Pokemon';
+
+export const Home = () => {
+  return (
+    <SafeAreaView>
+      <Pokemon />
+    </SafeAreaView>
+  );
+};
