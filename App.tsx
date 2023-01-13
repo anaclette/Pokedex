ed7e2320ec93c9bedd2c0f4c0cb6f532540e1986
ana.laclau <ana.laclau@mediamonks.com>
2023-01-13 16:14:15 -0300

    added tabs navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import StackNavigator from './src/navigation/StackNavigator';
 import {NavigationContainer} from '@react-navigation/native';
+import Tabs from './src/navigation/Tabs';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <Tabs />
     </NavigationContainer>
   );
 };
