345fac9d49e26c45809bed92cfb63768a91cec36
ana.laclau <ana.laclau@mediamonks.com>
2023-01-13 13:11:10 -0300

    useFullPokemon custom hook to retrieve full details

---
+++ src/hooks/useFullPokemon.ts
@@ -0,0 +1,25 @@
+import {useEffect, useState} from 'react';
+import {api} from '../api/pokemon';
+import {baseUrl} from '../common/constants';
+import {PokemonFullDetails} from '../types/Pokemon';
+
+export const useFullPokemon = (id: string) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [fullPokemon, setFullPokemon] = useState<PokemonFullDetails>(
+    {} as PokemonFullDetails,
+  );
+
+  const loadFullPokemon = async () => {
+    const result = await api.get<PokemonFullDetails>(`${baseUrl}/${id}`);
+
+    setFullPokemon(result.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadFullPokemon();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {isLoading, fullPokemon};
+};
