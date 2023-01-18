6f70467f55add0edc1e590d49ac8c7196f2eac5d
ana.laclau <ana.laclau@mediamonks.com>
2023-01-18 13:48:38 -0300

    drawer screens layout improvements

--- src/navigation/DrawerNavigator/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator/DrawerNavigator.tsx
@@ -14,14 +14,43 @@ const Drawer = createDrawerNavigator<DrawerStackParams>();
 
 export const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator screenOptions={{title: ''}}>
+    <Drawer.Navigator
+      screenOptions={{
+        title: '',
+        sceneContainerStyle: {backgroundColor: 'rgba(255,255,255, 0.92)'},
+        headerTransparent: true,
+        headerTintColor: '#fff',
+      }}>
       <Drawer.Screen
-        options={{drawerLabel: 'Home'}}
+        options={{
+          drawerLabel: 'Home',
+
+          drawerActiveBackgroundColor: 'transparent',
+          drawerActiveTintColor: 'darkblue',
+          drawerInactiveTintColor: 'black',
+          drawerInactiveBackgroundColor: 'transparent',
+          drawerContentContainerStyle: {
+            backgroundColor: 'rgba(2,35,252,0.2)',
+            opacity: 0.5,
+            flex: 1,
+          },
+        }}
         name="Home"
         component={Home}
       />
       <Drawer.Screen
-        options={{drawerLabel: 'Pokedex'}}
+        options={{
+          drawerLabel: 'Pokedex',
+          drawerActiveBackgroundColor: 'transparent',
+          drawerActiveTintColor: 'darkblue',
+          drawerInactiveTintColor: 'black',
+          drawerInactiveBackgroundColor: 'transparent',
+          drawerContentContainerStyle: {
+            backgroundColor: 'rgba(2,35,252,0.2)',
+            opacity: 0.5,
+            flex: 1,
+          },
+        }}
         name="Pokedex"
         component={PokemonList}
       />
