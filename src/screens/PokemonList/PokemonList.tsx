0380df694f0315f34ad3e5ab8c3050aaf0f6a236
ana.laclau <ana.laclau@mediamonks.com>
2023-01-30 17:37:59 -0300

    left commented out TODO on pokemon list and add loader on refresh

--- src/screens/PokemonList/PokemonList.tsx
+++ src/screens/PokemonList/PokemonList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {ActivityIndicator, FlatList} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {ActivityIndicator, FlatList, RefreshControl} from 'react-native';
 import PokemonCard from '../../components/PokemonCard';
 import {usePokemon} from '../../hooks/usePokemon';
 import {NewListPokemon} from '../../types/Pokemon';
@@ -13,7 +13,8 @@ import {height} from '../../common/constants';
 interface Props extends StackScreenProps<RootStackParams, 'PokemonDetails'> {}
 
 export const PokemonList = ({navigation, route}: Props) => {
-  const {pokeList, loadPokemons} = usePokemon();
+  const {pokeList, loadPokemons, isLoading} = usePokemon();
+  const [refreshing, setRefreshing] = useState(false);
   const {top} = useSafeAreaInsets();
   const pokePosterHeight = height / 4;
 
@@ -29,25 +30,48 @@ export const PokemonList = ({navigation, route}: Props) => {
     );
   };
 
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
+  const loading = refreshing || isLoading;
+
+  const showLoader = (isFooter?: boolean) => (
+    <ActivityIndicator
+      size={20}
+      color={'darkblue'}
+      style={isFooter ? styles.footerLoader : styles.loader}
+    />
+  );
+
   return (
     <>
       <PokemonPoster viewTop={top} imgHeight={pokePosterHeight} />
-      <FlatList
-        numColumns={2}
-        contentContainerStyle={{top: top + pokePosterHeight}}
-        showsVerticalScrollIndicator={false}
-        data={pokeList.sort(
-          (pokeA, pokeB) => pokeA.name.localeCompare(pokeB.name),
-          // TODO: fix how this is currently working
-        )}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={renderItem}
-        onEndReached={loadPokemons}
-        onEndReachedThreshold={0.4}
-        ListFooterComponent={
-          <ActivityIndicator size={20} color={'green'} style={styles.loader} />
-        }
-      />
+      {!loading ? (
+        <FlatList
+          numColumns={2}
+          contentContainerStyle={{top: top + pokePosterHeight}}
+          showsVerticalScrollIndicator={false}
+          // data={pokeList.sort(
+          //   (pokeA, pokeB) => pokeA.name.localeCompare(pokeB.name),
+          //    TODO: fix how this is currently working
+          // TODO: concat next page results onEndReached once data is retrieved from state
+          // )}
+          data={pokeList}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={renderItem}
+          onEndReached={loadPokemons}
+          onEndReachedThreshold={0.4}
+          onRefresh={onRefresh}
+          refreshing={refreshing}
+          ListFooterComponent={showLoader(true)}
+        />
+      ) : (
+        showLoader()
+      )}
     </>
   );
 };
--- src/screens/PokemonList/pokemonList.style.ts
+++ src/screens/PokemonList/pokemonList.style.ts
@@ -2,6 +2,10 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   loader: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  footerLoader: {
     justifyContent: 'flex-start',
     paddingTop: '30%',
     paddingBottom: 500,
