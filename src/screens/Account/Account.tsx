cd9fe90240f6bf280df3503817120872b1818d01
ana.laclau <ana.laclau@mediamonks.com>
2023-01-30 12:12:30 -0300

    fix account structure and styles

--- src/screens/Account/Account.tsx
+++ src/screens/Account/Account.tsx
@@ -1,19 +1,113 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, ImageBackground, TextInput} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {isIos} from '../../common/constants';
+import {styles} from './account.style';
 
 export const Account = () => {
   const {top} = useSafeAreaInsets();
 
   const topValue = isIos ? top : top + 10;
   return (
-    <View
-      style={{
-        backgroundColor: 'rgba(255,255,255, 0.92)',
-        flex: 1,
-      }}>
-      <Text style={{top: topValue}}>My account</Text>
-    </View>
+    <ImageBackground
+      source={require('../../assets/images/pikachu.jpeg')}
+      style={styles.container}>
+      <View style={styles.loginWrapper}>
+        {!username && (
+          <>
+            <View style={styles.textInputWrapper}>
+              <TextInput
+                maxLength={20}
+                autoFocus={true}
+                accessible={true}
+                accessibilityLabel="username input"
+                autoCorrect={false}
+                keyboardType="default"
+                style={styles.textInput}
+                placeholderTextColor={'darkRed'}
+                placeholder="username"
+                onChangeText={input => {
+                  checkIfEmpty(input, setEmptyField);
+                  setUserInput(input);
+                }}
+              />
+            </View>
+            <View style={styles.warningContainer}>
+              <Text
+                accessible={true}
+                accessibilityHint={
+                  'Validates if username is not empty and is longer than 4 characters.'
+                }
+                style={styles.warningText}>
+                {userInput !== '' &&
+                  userInput.length < minLength &&
+                  'Username must be at least five characters long.'}
+                {emptyField && 'Username is required'}
+              </Text>
+              <Text
+                accessible={true}
+                accessibilityHint={
+                  'Validates if username has no special characters.'
+                }
+                style={styles.warningText}>
+                {userInput !== '' &&
+                  validateUserInput(userInput) &&
+                  'Alphanumeric characters only.'}
+              </Text>
+            </View>
+            <Button
+              style={styles.button}
+              underlayColor="gray"
+              disabled={isDisabled()}
+              accessibilityState={{disabled: isDisabled()}}
+              accessibilityLabel={'Sign in'}
+              activeOpacity={0.8}
+              onPress={() => dispatch(logIn(userInput))}
+              children={<Text style={styles.buttonText}>Sign in</Text>}
+            />
+          </>
+        )}
+        {username && (
+          <View style={styles.diffAccountContainer}>
+            <View style={styles.diffAccountContainer}>
+              <Text
+                style={{...styles.storedUserName, ...styles.userInput}}
+                accessible={true}
+                accessibilityHint={
+                  'Verifies if current user matches logged in.'
+                }>
+                {handleUserMessage(username)}
+              </Text>
+              <Button
+                style={styles.button}
+                underlayColor="gray"
+                accessibilityLabel={'Log out'}
+                activeOpacity={0.8}
+                onPress={() => dispatch(logOut())}
+                children={<Text style={styles.buttonText}>Log out</Text>}
+              />
+            </View>
+            <View style={styles.diffUserNameWrapper}>
+              <Text
+                style={{...styles.diffUserNameMessage, ...styles.userInput}}>
+                {handleUserMessage(username, true)}
+              </Text>
+              <Button
+                style={styles.button}
+                underlayColor="gray"
+                accessibilityLabel={'Sign with a different account.'}
+                activeOpacity={0.8}
+                onPress={() => dispatch(logOut())}
+                children={
+                  <Text style={styles.buttonText}>
+                    Sign with a different account.
+                  </Text>
+                }
+              />
+            </View>
+          </View>
+        )}
+      </View>
+    </ImageBackground>
   );
 };
--- src/screens/Account/account.style.ts
+++ src/screens/Account/account.style.ts
@@ -1,3 +1,73 @@
 import {StyleSheet} from 'react-native';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255, 0.92)',
+    opacity: 0.5,
+  },
+  loginWrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  warningContainer: {
+    flexBasis: '10%',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  warningText: {
+    color: '#fff',
+    fontSize: 16,
+    fontFamily: 'SourceSansPro-Bold',
+  },
+  diffAccountContainer: {
+    justifyContent: 'space-between',
+    height: '65%',
+  },
+
+  textInput: {
+    color: 'black',
+    fontSize: 20,
+    textTransform: 'capitalize',
+    fontFamily: 'SourceSansPro-Regular',
+    padding: 5,
+    backgroundColor: 'rgba(255,255,255, 0.92)',
+    borderRadius: 10,
+  },
+  textInputWrapper: {
+    width: '50%',
+  },
+
+  storedUserName: {
+    fontSize: 30,
+    top: '30%',
+  },
+  diffUserNameWrapper: {
+    bottom: '10%',
+  },
+  userInput: {
+    color: '#fff',
+    fontFamily: 'Quicksand-Bold',
+    textAlign: 'center',
+  },
+  diffUserNameMessage: {
+    fontSize: 20,
+  },
+  button: {
+    borderRadius: 20,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#fff',
+    alignSelf: 'center',
+    justifyContent: 'flex-end',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+    marginVertical: 10,
+    textAlign: 'center',
+    fontFamily: 'SourceSansPro-Regular',
+    paddingHorizontal: 10,
+  },
+});
