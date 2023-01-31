4bfe191636b890dce0a08c9130c1d1a8724cc94f
ana.laclau <ana.laclau@mediamonks.com>
2023-01-31 18:09:25 -0300

    rename drawer screen

--- src/navigation/DrawerNavigator/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator/DrawerNavigator.tsx
@@ -43,7 +43,7 @@ export const DrawerNavigator = () => {
       />
       <Drawer.Screen
         options={{
-          drawerLabel: 'Poke list',
+          drawerLabel: 'Pokedex',
           drawerLabelStyle: {
             fontFamily: 'Lora-Bold',
           },
