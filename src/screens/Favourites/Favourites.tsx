01ab18fa3cf73a53ae921ba71b5cbccf00be373a
ana.laclau <ana.laclau@mediamonks.com>
2023-02-06 14:36:04 -0300

    add delete all favourites button

--- src/screens/Favourites/Favourites.tsx
+++ src/screens/Favourites/Favourites.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, FlatList, Image, ImageBackground, View} from 'react-native';
 import {useTranslation} from 'react-i18next';
 import {useFavourites} from '../../utils/hooks';
@@ -7,13 +7,15 @@ import {StackScreenProps} from '@react-navigation/stack';
 import {RootStackParams} from '../../navigation/StackNavigator/StackNavigator';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {styles} from './favourites.style';
-// import {useAppSelector} from '../../state/hooks';
 import {TranslationKeys} from '../../locale/translations/keys';
 import Pokeball from '../../components/Pokeball';
 import {isIos} from '../../common/constants';
 import metrics from '../../themes/metrics';
 import Button from '../../components/Button';
 import colors from '../../themes/colors';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useAppDispatch} from '../../state/hooks';
+import {toggleIsFavourite} from '../../state/reducers/favouritesReducer';
 
 interface Props
   extends StackScreenProps<RootStackParams, 'PokemonDetails', ''> {}
@@ -24,9 +26,43 @@ export const Favourites = ({navigation, route}: Props) => {
   const {t} = useTranslation();
   const {bottom} = useSafeAreaInsets();
   // const languageOptions = useAppSelector(state => state.languages.lang); // TODO: get language endpoint from data
+  const dispatch = useAppDispatch();
+
+  const removeFavourites = () => {
+    favourites.forEach(favouritePokemon => {
+      favourites.length !== 0 && dispatch(toggleIsFavourite(favouritePokemon));
+    });
+  };
 
   const showTitle = () => (
-    <Text style={styles.title}>{t(TranslationKeys.MY_FAVOURITES)}</Text>
+    <View style={styles.titleWrapper}>
+      {favourites.length !== 0 && (
+        <Button
+          accessibilityRole="button"
+          accessibilityLabel={t(TranslationKeys.REMOVE_ALL_FAVOURITES)}
+          activeOpacity={0.6}
+          onPress={() => removeFavourites()}
+          underlayColor={colors.transparent}
+          children={
+            <Icon
+              accessibilityRole="image"
+              name="delete"
+              size={metrics.scale(20)}
+              color={colors.dark}
+            />
+          }
+        />
+      )}
+      <Text
+        style={{
+          fontSize: isIos
+            ? metrics.scaledFontSize(35)
+            : metrics.scaledFontSize(50),
+          ...styles.title,
+        }}>
+        {t(TranslationKeys.MY_FAVOURITES)}
+      </Text>
+    </View>
   );
   return favourites?.length ? (
     <>
@@ -43,10 +79,8 @@ export const Favourites = ({navigation, route}: Props) => {
         numColumns={2}
         data={favourites}
         contentContainerStyle={{
-          top: isIos ? top * 3 : top + metrics.scaleVertical(130),
-          paddingBottom: isIos
-            ? bottom + metrics.scaleVertical(200)
-            : bottom + metrics.scaleVertical(250),
+          top: top + metrics.scaleVertical(130),
+          paddingBottom: bottom + metrics.scaleVertical(250),
         }}
         renderItem={({item}) => (
           <PokemonCard
