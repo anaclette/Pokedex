14dc22dfdb3b36e1e752cd46ece72d8b80af3ce6
ana.laclau <ana.laclau@mediamonks.com>
2023-02-07 17:24:59 -0300

    replace window dimensions constants import

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,10 +1,9 @@
-import {Platform, Dimensions} from 'react-native';
+import {Platform} from 'react-native';
 
 export const baseUrl = 'https://pokeapi.co/api/v2';
 export const pictureBaseUrl =
   'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork';
 export const isIos = Platform.OS === 'ios';
-export const {height, width} = Dimensions.get('window');
 
 export enum STATE_MODULES {
   AUTH = 'auth',
--- src/themes/metrics.ts
+++ src/themes/metrics.ts
@@ -1,15 +1,24 @@
-import {s as RNScale, vs as RNVerticalScale} from 'react-native-size-matters';
-import {height} from '../common/constants';
-
-const scale = (size: number) => RNScale(size);
-const scaleVertical = (size: number) => RNVerticalScale(size);
+import {Dimensions} from 'react-native';
+import {vs as RNVerticalScale} from 'react-native-size-matters';
+export const {height, width, fontScale} = Dimensions.get('window');
 
 const HEIGHT_CUT_OFF = 600;
 const scaledFontSize = (size: number) =>
   height > HEIGHT_CUT_OFF ? size : size * (height / (HEIGHT_CUT_OFF * 1.1));
+const scaleVertical = (size: number) => RNVerticalScale(size);
+
+const guidelineBaseWidth = 350;
+const guidelineBaseHeight = 680;
+
+const scale = (size: number) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size: number) => (height / guidelineBaseHeight) * size;
+const moderateScale = (size: number, factor = 0.5) =>
+  size + (scale(size) - size) * factor;
 
 export default {
   scaledFontSize,
   scale,
   scaleVertical,
+  verticalScale,
+  moderateScale,
 };
