d95a92fc621904097b55fb7885ab2ddb3dcc4d10
ana.laclau <ana.laclau@mediamonks.com>
2023-02-09 11:45:43 -0300

    replace language buttons and fix onpress not working

--- src/components/LanguageButtons/LanguageButtons.tsx
+++ src/components/LanguageButtons/LanguageButtons.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
-import {Image, Text, View} from 'react-native';
+import {Image, View} from 'react-native';
 import {useAppDispatch, useAppSelector} from '../../state/hooks';
 import {switchLanguage} from '../../state/reducers/i18nReducer';
 import colors from '../../themes/colors';
@@ -17,10 +17,7 @@ export const LanguageButtons = () => {
   return (
     <View style={styles.container}>
       <Button
-        style={{
-          ...styles.topButtonWrapper,
-          ...styles.languageOptionWrapper,
-        }}
+        style={styles.languageOptionWrapper}
         accessibilityLabel={t(TranslationKeys.FLAG_BUTTON_IMAGE)}
         accessibilityRole="button"
         activeOpacity={0.5}
@@ -30,34 +27,25 @@ export const LanguageButtons = () => {
           i18n.changeLanguage(languageOptions.es);
         }}
         children={
-          <>
-            <Image
-              style={styles.flagImage}
-              source={require('../../assets/images/argentina_flag.jpeg')}
-            />
-            <Text style={styles.languageOptionText}>ESP</Text>
-          </>
+          <Image
+            style={styles.flagImage}
+            source={require('../../assets/images/argentina_flag.jpeg')}
+          />
         }
       />
-      {/* TODO: fix second button onPress not working */}
+
       <Button
-        style={{
-          ...styles.bottomButtonWrapper,
-          ...styles.languageOptionWrapper,
-        }}
+        style={styles.languageOptionWrapper}
         accessibilityLabel={t(TranslationKeys.FLAG_BUTTON_IMAGE)}
         accessibilityRole="button"
         activeOpacity={0.5}
         underlayColor={colors.transparent}
         onPress={() => console.log('elegiste idioma extranjero')}
         children={
-          <>
-            <Image
-              style={styles.flagImage}
-              source={require('../../assets/images/USA_flag.webp')}
-            />
-            <Text style={styles.languageOptionText}>ENG</Text>
-          </>
+          <Image
+            style={styles.flagImage}
+            source={require('../../assets/images/USA_flag.webp')}
+          />
         }
       />
     </View>
--- src/components/LanguageButtons/languageButtons.style.ts
+++ src/components/LanguageButtons/languageButtons.style.ts
@@ -1,33 +1,23 @@
 import {StyleSheet} from 'react-native';
 import colors from '../../themes/colors';
-import fonts from '../../themes/fonts';
 import metrics from '../../themes/metrics';
 
 export const styles = StyleSheet.create({
   container: {
     position: 'absolute',
-    left: 0,
-    top: '10%',
+    top: '8%',
+    right: 0,
+    zIndex: 1,
   },
   languageOptionWrapper: {
     borderColor: colors.translucidLightBackground,
-  },
-  topButtonWrapper: {
-    borderTopWidth: metrics.scale(1),
-    borderRightWidth: metrics.scale(1),
-    borderBottomWidth: metrics.scale(1),
-  },
-  bottomButtonWrapper: {
-    borderBottomWidth: metrics.scale(1),
-    borderRightWidth: metrics.scale(1),
+    borderRadius: metrics.scale(20),
+    borderWidth: metrics.scale(1),
+    marginVertical: metrics.verticalScale(1),
   },
   flagImage: {
     width: metrics.moderateScale(30, 0.4),
     height: metrics.moderateScale(30, 0.4),
-  },
-  languageOptionText: {
-    textAlign: 'center',
-    color: colors.translucidLightBackground,
-    fontFamily: fonts.itemBold,
+    borderRadius: metrics.scale(20),
   },
 });
