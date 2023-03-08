ae63e38220e8074a2cb1b44b5546176bf6217d31
ana.laclau <ana.laclau@mediamonks.com>
2023-03-08 17:19:38 -0300

    add jest setup file to mock async storage and persist

---
+++ jestSetup.ts
@@ -0,0 +1,15 @@
+export {};
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
+jest.mock('redux-persist', () => {
+  const real = jest.requireActual('redux-persist');
+  return {
+    ...real,
+    persistReducer: jest
+      .fn()
+      .mockImplementation((config, reducers) => reducers),
+  };
+});
