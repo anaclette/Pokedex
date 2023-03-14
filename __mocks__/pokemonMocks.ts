4348acf844ee8138d757427f70a75b823772a39d
ana.laclau <ana.laclau@mediamonks.com>
2023-03-14 11:21:40 -0300

    create pokemon mocks

---
+++ __mocks__/pokemonMocks.ts
@@ -0,0 +1,80 @@
+import {
+  Species,
+  Ability,
+  Move,
+  Sprites,
+  Stat,
+  Type,
+  PokemonFullDetails,
+} from '../src/types/Pokemon';
+
+const speciesMock: Species = {
+  name: 'Pikachu',
+  url: 'url',
+};
+
+const abilitiesMock: Ability = {
+  ability: speciesMock,
+  is_hidden: false,
+  slot: 1,
+};
+
+const movesMock: Move = {
+  move: speciesMock,
+};
+
+const spritesMock: Sprites = {
+  back_default: 'back-default-url',
+  back_female: null,
+  back_shiny: 'back-shiny-url',
+  back_shiny_female: null,
+  front_default: 'front-default-url',
+  front_female: null,
+  front_shiny: 'front-shiny-url',
+  front_shiny_female: null,
+};
+
+const statsMock: Stat = {
+  base_stat: 20,
+  effort: 30,
+  stat: speciesMock,
+};
+
+const typesMock: Type = {
+  slot: 1,
+  type: speciesMock,
+};
+
+const pokemonMock: PokemonFullDetails = {
+  abilities: [abilitiesMock],
+  base_experience: 50,
+  forms: [speciesMock],
+  height: 50,
+  held_items: [],
+  id: '3',
+  is_default: false,
+  location_area_encounters: 'location-area-encounters',
+  moves: [movesMock],
+  name: 'Pikachu',
+  order: 10,
+  past_types: [],
+  species: speciesMock,
+  sprites: spritesMock,
+  stats: [statsMock],
+  types: [typesMock],
+  weight: 100,
+};
+
+// const pokemonDetailsMock = {
+//   pokemonDetails: {
+//     id: '3',
+//     name: 'Charmander',
+//     picture: 'picture-url',
+//     imgColor: 'green',
+//     textColor: 'white',
+//   },
+//   imgColor: 'green',
+//   textColor: 'white',
+// };
+
+export {pokemonMock, abilitiesMock, speciesMock};
